b24e458bb1c30a6ad9fddb4c23791c94e17ac3ad
tzmanics <tzmanics@gmail.com>
2021-02-14 23:02:54 -0500

    changes project name to angular-sanity

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-universal-pre-render'`, () => {
+  it(`should have as title 'angular-sanity'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-universal-pre-render');
+    expect(app.title).toEqual('angular-sanity');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-universal-pre-render app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'angular-sanity app is running!'
+    );
   });
 });
